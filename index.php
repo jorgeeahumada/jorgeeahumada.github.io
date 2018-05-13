356832cb654b0da0f8392b9caf738e86e45ab429
Jorge Eduardo Ahumada <e.ahumada.hdz@gmail.com>
2018-05-12 18:56:52 -0700

    Made all the files php files and trying to use fundations js for the navigation menu button

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Ahumada-Home</title>
+    <meta name="author" content="Jorge Eduardo Ahumada">
+    <meta name="description" content="Introduction to the professional portfolio of Jorge Eduaro Ahumada">
+    <meta name="keywords" content="web design, user experience, informatics, front end development, full
+        stack development, portfolio, responsive design">
+    <link rel="icon" type="image/png" href="images/ux-favicon.ico">
+    <link rel="stylesheet" href="css/foundation.min.css"></link>
+    <link rel="stylesheet" href="fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <script src="js/vendor/foundation.min.js"></script>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+</head>
+<body>
+<?php
+ include navbar.php;
+ ?>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: e_ahu
+ * Date: 5/12/2018
+ * Time: 6:53 PM
+ */
+?>
+<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+    <button type="button" data-toggle="example-menu"><i class="fas fa-bars"</button>
+</div>
